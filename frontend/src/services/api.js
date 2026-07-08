e1b4ec19ed93367fd9800622138201fcc1e5a3c2
Chetan Tyagi <tyagichetan443@gmail.com>
2026-07-09 00:06:15 +0530

    Configure API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Attach token automatically
@@ -15,9 +15,7 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
